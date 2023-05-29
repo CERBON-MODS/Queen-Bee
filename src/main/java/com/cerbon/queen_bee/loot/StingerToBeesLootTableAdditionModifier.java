4e8530477bed17411b436f093238f2c3cec28047
Felipe Cerboncini <felipecerboncinix@gmail.com>
2023-05-29 18:36:54 -0300

    Add stinger item drop to any entity instanceof bee

--- src/main/java/com/cerbon/queen_bee/loot/StingerToBeesLootTableAdditionModifier.java
+++ src/main/java/com/cerbon/queen_bee/loot/StingerToBeesLootTableAdditionModifier.java
@@ -3,9 +3,11 @@ package com.cerbon.queen_bee.loot;
 import com.google.gson.JsonObject;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
@@ -31,8 +33,17 @@ public class StingerToBeesLootTableAdditionModifier extends LootModifier {
     @NotNull
     @Override
     protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.add(new ItemStack(addition, 1));
+        if (checkIfInjectLoot(context)){
+            generatedLoot.add(new ItemStack(addition, 1));
+        }
         return generatedLoot;
+    }
+
+    public static boolean checkIfInjectLoot(LootContext context) {
+        if (context.hasParam(LootContextParams.THIS_ENTITY)) {
+            return context.getParam(LootContextParams.THIS_ENTITY) instanceof Bee;
+        }
+        return false;
     }
 
     public static class Serializer extends GlobalLootModifierSerializer<StingerToBeesLootTableAdditionModifier>{
